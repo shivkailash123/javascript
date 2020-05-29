f354c13f9b7ee8ba88f84a533ca2b34d374c500f
shiv tiwari <shiv.tiwari@mindtree.com>
2020-05-29 16:23:58 +0530

    javascript practice

---
+++ Rectangle.js
@@ -0,0 +1,10 @@
+    class Reactangle{
+        constructor(){
+            console.log("This is rectangle method")
+            this.width = 5;
+            this.height = 8;
+            this.color = "blue";
+        }
+    }
+    console.log("hello world")
+    let myRectangle = new Reactangle();
